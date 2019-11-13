bdc9ac732e7dc3fd0b823b2f46ed092d25b2c0ca
laislopes <laisjkl@hotmail.com>
2019-11-13 20:41:06 -0200

    test

--- src/Infra/ConexaoFactory.java
+++ src/Infra/ConexaoFactory.java
@@ -7,9 +7,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoFactory {
-	private static final String USUARIO = "bdlembrete";
-	private static final String SENHA = "masterkey";
-	private static final String URL = "jdbc:mysql://localhost:3306/lembrete";
+	private static final String USUARIO = "root";
+	private static final String SENHA = "root";
+	private static final String URL = "jdbc:mysql://localhost:3306/reminders";
 	
 	public static Connection conectar() throws SQLException {
 		
